463d369fcfa3934ad3a8c81379ccb777e443e61c
Mateus Oliveira <mateusoliveira.555@proton.me>
2024-05-07 16:46:10 -0300

    adding installer.sh file

--- i3installer.sh
+++ i3installer.sh
@@ -3,6 +3,6 @@
 # i3 essential
 
 apt install xorg \
-			i3 \
-			i3blocks \
-			lightdm
+	i3 \
+	i3blocks \
+	lightdm
---
+++ installer.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "type --help to get help"
+	exit 0
+else
+	for arg in $@; do
+		if [ $arg == "--help" ]; then
+			echo "run with sudo or as root!"
+			echo "installer.sh [options]: enter one or more options"
+			echo "--system -> system utilities"
+			echo "--dev    -> development tools"
+			echo "--midea  -> multimedia applications"
+			echo "--web    -> browser and utilities"
+			echo "--creat  -> creativity tools"
+			echo "--help   -> help"
+			exit 0 
+		fi
+	done
+fi
+
+apt updade
+
+for arg in $@; do
+	case $arg in
+		"--system")
+			apt install -y xfce4-terminal \
+				neofetch \
+				rsync \
+				nitrogen \
+				curl \
+				htop \
+				pcmanfm
+			;;
+		"--dev")
+			apt install -y git \
+				vim \
+				build-essential \
+				simulide
+			;;
+		"--midea")
+			apt install -y mirage \
+				antril \
+				vlc
+			;;
+		"--web")
+			apt install -y firefox-esr \
+				transmission
+			;;
+		"--creat")
+			apt install -y musescore \
+				gimp \
+				inkscape
+			;;
+		*)
+			echo "$arg invalid option"
+			;;
+
+	esac
+done
